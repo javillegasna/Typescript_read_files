c6dd728eee5213de4c8aea06e2afddc39442a23b
Javier Villegas <javillegasna@gmail.com>
2022-06-08 15:33:01 -0500

    [REFCT] Refactor of test from data to object process

--- src/utils/utilites.test.ts
+++ src/utils/utilites.test.ts
@@ -5,47 +5,41 @@ test("Read file from relative path", () => {
   expect(readFile(config.FILE_PATH)).toMatch(/(INPUT)/i);
   expect(readFile("..")).toBe(false);
 });
+describe("Data string to nested object process", () => {
+  const { SET_SPLITTER: SS, KEY_SPLITTER: KS } = config;
+  const { INTERVAL_SPLITTER: IS, DATE_SPLITTER: DS } = config;
+  const testObject = {
+    Set1: [
+      { RENE: { MO: ["10:00", "12:00"] } },
+      { RENE: { MO: ["10:00", "12:00"] } },
+    ],
+    Set2: [{ RENE: { MO: ["10:00", "12:00"], TU: ["10:00", "12:00"] } }],
+  };
+  test("when the input is Array of dates, the output will be an object  ", () => {
+    //Date structure DayHour1-Hour2 => DDH1-H2
+    const { RENE: set1 } = testObject.Set1[0];
+    const {RENE: set2}= testObject.Set2[0]
+    expect(dateTree(["MO10:00" + IS + "12:00"])).toMatchObject(set1);
+    expect(
+      dateTree(["MO10:00" + IS + "12:00", "TU10:00" + IS + "12:00"])
+    ).toMatchObject(set2);
+  });
 
-test("when the input is Array of dates, the output will be an object  ", () => {
-  //Date structure DayHour1-Hour2 => DDH1-H2
-  const { INTERVAL_SPLITTER: IS } = config;
-  expect(dateTree(["MO10:00" + IS + "12:00"])).toMatchObject({
-    MO: ["10:00", "12:00"],
+  test("If the input was a data row the output should be a nested Object  ", () => {
+    //SetRow structure Name=DayHour1-Hour2 => N=DDH1-H2
+    const [set1_name1] = testObject.Set1
+    const [set2_name] = testObject.Set2
+    const DATE = `10:00${IS}12:00`;
+    expect(objectBuilder(`RENE${KS}MO${DATE}`)).toMatchObject(set1_name1);
+    expect(objectBuilder(`RENE${KS}MO${DATE + DS}TU${DATE}`)).toMatchObject(set2_name);
   });
-  expect(
-    dateTree(["MO10:00" + IS + "12:00", "TU10:00" + IS + "12:00"])
-  ).toMatchObject({
-    MO: ["10:00", "12:00"],
-    TU: ["10:00", "12:00"],
-  });
-});
 
-test("If the input was a data row the output should be a nested Object  ", () => {
-  const { KEY_SPLITTER: KS, INTERVAL_SPLITTER: IS, DATE_SPLITTER: DS } = config;
-  const DATE = `10:00${IS}12:00`;
-  expect(objectBuilder(`RENE${KS}MO${DATE}`)).toMatchObject({
-    RENE: { MO: ["10:00", "12:00"] },
+  test("If the input was a data string, the output should be an Array of nested Objects", () => {
+    //Set structure INPUT\nSetRow\nSetRow => INPUT\nN=DDH1-H2\nN=DDH1-H2
+    //SetRow structure Name=DayHour1-Hour2 => N=DDH1-H2
+    const DATE = `10:00${IS}12:00`;
+    const template = `${SS}RENE${KS}MO${DATE}`;
+    expect(transformData(template)).toEqual([testObject.Set1[0]]);
+    expect(transformData(template + template)).toEqual(testObject.Set1);
   });
-  expect(objectBuilder(`RENE${KS}MO${DATE + DS}TU${DATE}`)).toMatchObject({
-    RENE: { MO: ["10:00", "12:00"], TU: ["10:00", "12:00"] },
-  });
-});
-
-test("If the input was a data string, the output should be an Array of nested Objects", () => {
-  const {
-    SET_SPLITTER: SS,
-    ROW_SPLITTER: RS,
-    KEY_SPLITTER: KS,
-    INTERVAL_SPLITTER: IS,
-    DATE_SPLITTER: DS,
-  } = config;
-  const DATE = `10:00${IS}12:00`;
-  const template = `${SS}RENE${KS}MO${DATE}`;
-  expect(transformData(template)).toEqual([
-    { RENE: { MO: ["10:00", "12:00"] } },
-  ]);
-  expect(transformData(template+template)).toEqual([
-    { RENE: { MO: ["10:00", "12:00"] } },
-    { RENE: { MO: ["10:00", "12:00"] } },
-  ]);
 });
